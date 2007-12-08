0bd4408de0f7055dff02a26902772980037b2a11
Pebbles <Pebbles@d71109c7-5459-df5a-5d14-810f3b78ceb1>
2007-12-08 19:27:12 +0000

    Tagged 0.9.5 release.

---
+++ tags/release-0-9-5/Binaries/Installer-Release-x86.nsi
@@ -0,0 +1,9 @@
+;; $Id$
+
+!define BUILDDIR Release
+!define BUILD    Release
+!define PLATFORM x86
+
+InstallDir "$PROGRAMFILES32\Pebbles\POLUtils"
+
+!include "Installer.nsh"
---
+++ tags/release-0-9-5/ItemListUpgrade/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿// $Id$
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyTitle        ("ItemListUpgrade")]
+[assembly: AssemblyDescription  ("A utility to upgrade XML item dumps to the format used by the current version of POLUtils.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany      ("")]
+[assembly: AssemblyProduct      ("POLUtils")]
+[assembly: AssemblyCopyright    ("Copyright © 2005,2006,2007 Tim Van Holder")]
+[assembly: AssemblyTrademark    ("")]
+[assembly: AssemblyCulture      ("")]
+
+[assembly: AssemblyVersion      ("0.9.5.0")]
